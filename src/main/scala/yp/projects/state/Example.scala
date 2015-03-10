cc0ecb6fe0f69cfd48ed5ad91cb9ff02f750d641
Yeghishe Piruzyan <ypiruzyan@gmail.com>
2015-03-10 11:42:18 -0700

    commiting what I have

---
+++ src/main/scala/yp/projects/state/Example.scala
@@ -0,0 +1,97 @@
+package yp.projects.state
+
+import scalaz._
+import Scalaz._
+/*
+import scalaz.syntax.std.option._
+import scalaz.State
+import scalaz.Monoid
+import scalaz.syntax.traverse._
+import scalaz.std.list._
+*/
+
+/**
+ * Created by yeghishe on 2/11/15.
+ */
+object Example {
+  type StateCache[A] = State[Cache, A]
+
+  case class Timestamped[A](value: A, timestamp: Long)
+
+  case class FollowerStats(
+    username: String,
+    numFollowers: Int,
+    numFollowing: Int
+  )
+
+  case class Cache(
+    stats: Map[String, Timestamped[FollowerStats]],
+    hits: Int,
+    misses: Int
+  ) {
+    def get(username: String): Option[Timestamped[FollowerStats]] =
+      stats.get(username)
+
+    def update(username: String, followerStats: Timestamped[FollowerStats]): Cache =
+      copy(stats = stats + (username -> followerStats))
+
+    def recordHit: Cache = copy(hits = hits + 1)
+    def recordMiss: Cache = copy(misses = misses + 1)
+  }
+
+  implicit object CacheMonoid extends Monoid[Cache] {
+    override def zero: Cache =
+      Cache(Map.empty, 0, 0)
+
+    override def append(f1: Cache, f2: => Cache): Cache =
+      Cache(f1.stats ++ f2.stats, f1.hits + f2.hits, f1.misses + f2.misses)
+  }
+
+  trait SocialService {
+    def followerStats(username: String): StateCache[FollowerStats]
+  }
+
+  object SocialService {
+    implicit object SocialServiceImpl extends SocialService {
+      override def followerStats(username: String): StateCache[FollowerStats] = for {
+        ofs <- checkCache(username)
+        fs <- ofs.fold(receive(username))(State.state)
+      } yield fs
+
+      private def checkCache(username: String): StateCache[Option[FollowerStats]] = for {
+        ofs <- State.gets { s: Cache =>
+          s.get(username).collect { case Timestamped(fs, ts) if !stale(ts) => fs }
+        }
+        _ <- State.modify[Cache] { s => ofs ? s.recordHit | s.recordMiss }
+      } yield ofs
+
+      private def stale(ts: Long): Boolean = System.currentTimeMillis() - ts > (5 * 60 * 1000L)
+
+      private def receive(username: String): StateCache[FollowerStats] = for {
+        fs <- State.state(callWebService(username))
+        tfs = Timestamped(fs, System.currentTimeMillis())
+        _ <- State.modify[Cache](s => s.update(username, tfs))
+      } yield fs
+
+      private def callWebService(username: String): FollowerStats = FollowerStats(username, 0, 0)
+    }
+  }
+}
+
+import Example._
+
+object DemoExample {
+
+
+  def main(args: Array[String]): Unit = {
+    val socialService = implicitly[SocialService]
+
+    val listOfState = List(socialService.followerStats("a"), socialService.followerStats("b"), socialService.followerStats("a"))
+    val stateOfList = listOfState.sequence[StateCache, FollowerStats]
+
+    //println(State.state[StateCache](10).runZero)
+    println(10.point[StateCache].runZero)
+
+    println(stateOfList.runZero)
+  }
+}
---
+++ src/main/scala/yp/projects/state/Metrics.scala
@@ -0,0 +1,96 @@
+package yp.projects.state
+
+import scalaz._
+import Scalaz._
+
+/**
+ * Created by yeghishe on 2/10/15.
+ */
+/*
+sealed trait Metrics {
+  def set(name: String, value: Int): Unit
+  def get(name: String): Option[Int]
+  def inc(name: String): Unit
+  def incBy(name: String, value: Int): Unit
+}
+
+object Metrics {
+  implicit object InMemoryMetrics extends Metrics {
+    private var data: Map[String, Int] = Map.empty
+
+    override def set(name: String, value: Int): Unit = data += name -> value
+
+    override def incBy(name: String, value: Int): Unit =
+      data += name -> (data.getOrElse(name, 0) + value)
+
+    override def inc(name: String): Unit = incBy(name, 1)
+
+    override def get(name: String): Option[Int] = data.get(name)
+  }
+}
+*/
+
+
+/*
+case class Data(map: Map[String, Int]) {
+  def get(name: String): Option[Int] = map.get(name)
+  def set(name: String, value: Int): Data = copy(map + (name -> value))
+  def incBy(name: String, value: Int): Data = copy(map + (name -> (map.getOrElse(name, 0) + value)))
+  def inc(name: String): Data = incBy(name, 1)
+}
+*/
+
+
+
+sealed trait Metrics {
+  type StateMetrics[A] = State[Map[String, Int], A]
+
+  def setValue(name: String, value: Int): StateMetrics[Unit]
+  def getValue(name: String): StateMetrics[Option[Int]]
+  def inc(name: String): StateMetrics[Unit]
+  def incBy(name: String, value: Int): StateMetrics[Unit]
+}
+
+object Metrics {
+  implicit object InMemoryMetrics extends Metrics {
+    override def setValue(name: String, value: Int): StateMetrics[Unit] =
+      State { s => (s + (name -> value), ()) }
+
+    override def incBy(name: String, value: Int): StateMetrics[Unit] =
+      State { s => (s + (name -> (s.getOrElse(name, 0) + value)), ()) }
+
+    override def inc(name: String): StateMetrics[Unit] =
+      incBy(name, 1)
+
+    override def getValue(name: String): StateMetrics[Option[Int]] =
+      State { s => (s, s.get(name)) }
+  }
+}
+
+object DemoMetrics {
+  def main (args: Array[String]) {
+
+    //import Metrics.StateMetrics
+
+    val metrics = implicitly[Metrics]
+    val listOfStates = List(metrics.setValue("a", 1), metrics.inc("a"), metrics.getValue("a"))
+    //val stateOfList = listOfStates.sequence[StateMetrics, ]
+
+    //metrics.getValue("a").
+
+
+    /*
+    val metrics = implicitly[Metrics]
+
+    for {
+      s <- metrics.setValue("a", 1)
+      x <- s.get
+    }
+    metrics.setValue("a", 1).
+      .get("a")
+      .eval(Map.empty)
+
+    println(x)
+    */
+  }
+}
